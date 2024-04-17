3bbb6d7150fac8912101bc51ddc498079ff787b4
Wagner Terry <goncalveswagner15@gmail.com>
2024-04-17 17:52:43 -0300

    chore: iniciando rotas

--- frontend/pokemon/src/App.tsx
+++ frontend/pokemon/src/App.tsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
 import { Home } from "./views/Home"
+import { Team } from "./views/Team";
 
 function App() {
-  return <Home />
+  const [currentPage, setCurrentPage] = useState('home');
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'home':
+        return <Home />;
+      case 'team':
+        return <Team />;
+      default:
+        return <Home />;
+    }
+  };
+
+  const handleNavigation = (page: string) => {
+    setCurrentPage(page);
+  };
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li><button onClick={() => handleNavigation('home')}>Home</button></li>
+          <li><button onClick={() => handleNavigation('team')}>Time</button></li>
+        </ul>
+      </nav>
+      {renderPage()}
+    </div>
+  )
 }
 
 export default App
