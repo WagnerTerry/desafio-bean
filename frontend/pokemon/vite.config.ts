ebfda6a2c50d4e6349e04a08fa4ed9b142778465
Wagner Terry <goncalveswagner15@gmail.com>
2024-04-16 23:14:22 -0300

    chore: configurando vite

--- frontend/pokemon/vite.config.ts
+++ frontend/pokemon/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  // definindo a porta , e se o host for true, o projeto fica em um endereço público
+  preview: {
+    host: true,
+    port: 8080,
+  },
+});
