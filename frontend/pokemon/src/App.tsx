349aee1f717059cdebdfc57054603cfb9bf6f8cc
Wagner Terry <goncalveswagner15@gmail.com>
2024-04-17 18:03:39 -0300

    feat: componente Navbar

--- frontend/pokemon/src/App.tsx
+++ frontend/pokemon/src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from "react";
-import { Home } from "./views/Home"
-import { Team } from "./views/Team";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('home');
-
-  const renderPage = () => {
-    switch (currentPage) {
-      case 'home':
-        return <Home />;
-      case 'team':
-        return <Team />;
-      default:
-        return <Home />;
-    }
-  };
-
-  const handleNavigation = (page: string) => {
-    setCurrentPage(page);
-  };
   return (
-    <div>
-      <nav>
-        <ul>
-          <li><button onClick={() => handleNavigation('home')}>Home</button></li>
-          <li><button onClick={() => handleNavigation('team')}>Time</button></li>
-        </ul>
-      </nav>
-      {renderPage()}
-    </div>
+    <>
+      <h1>Pokédex</h1>
+
+      <Navbar page={'home'} />
+    </>
   )
+
 }
 
 export default App
---
+++ frontend/pokemon/src/components/Navbar/index.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { PokemonList } from "../../views/PokemonList";
+import { Team } from "../../views/Team";
+
+interface NavbarProps {
+    page: string;
+}
+
+export function Navbar({ page }: NavbarProps) {
+    const [currentPage, setCurrentPage] = useState(page);
+
+    const renderPage = () => {
+        switch (currentPage) {
+            case 'home':
+                return <PokemonList />;
+            case 'team':
+                return <Team />;
+            default:
+                return <PokemonList />;
+        }
+    };
+
+    const handleNavigation = (page: string) => {
+        setCurrentPage(page);
+    };
+
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><button onClick={() => handleNavigation('list')}>Pokemon List</button></li>
+                    <li><button onClick={() => handleNavigation('team')}>Team</button></li>
+                </ul>
+            </nav>
+            {renderPage()}
+        </div>
+    );
+}
