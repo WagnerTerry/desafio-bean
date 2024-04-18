94a4f505012da30cd9e3012083191447c0f02500
Wagner Terry <goncalveswagner15@gmail.com>
2024-04-18 17:27:40 -0300

    feat: exibindo imagens dos pokemons

--- frontend/pokemon/src/services/PokeService.ts
+++ frontend/pokemon/src/services/PokeService.ts
@@ -1,10 +1,25 @@
 import axios from "axios";
 
-const BaseURL = "https://pokeapi.co/api/v2/pokemon/?limit=1302";
+const BaseURL = "https://pokeapi.co/api/v2/pokemon";
 
+interface IPokemon {
+  url: string;
+}
 export default class PokeService {
-  static getAllPokemons = async () => {
-    const result = await axios.get(BaseURL);
-    return result.data;
+  static gottaCatchAll = async () => {
+    const { data } = await axios.get(`${BaseURL}/?limit=151`);
+
+    const pokemonPromises = data.results.map((poke) => {
+      return this.getPokemonDetails(poke);
+    });
+
+    const pokemonDetails = await Promise.all(pokemonPromises);
+
+    return pokemonDetails;
+  };
+
+  static getPokemonDetails = async (pokemon: IPokemon) => {
+    const { data } = await axios.get(pokemon.url);
+    return data;
   };
 }
--- frontend/pokemon/src/views/PokemonList/index.tsx
+++ frontend/pokemon/src/views/PokemonList/index.tsx
@@ -1,3 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useEffect, useState } from "react"
+import PokeService from "../../services/PokeService"
+
 export function PokemonList() {
-    return <h1>PokemonList</h1>
+    // interface IPoke {
+    //     results: [];
+    // }
+
+    const [list, setList] = useState<any[]>([]);
+
+    useEffect(() => {
+        const showPokemons = async () => {
+            const pokemons = await PokeService.gottaCatchAll()
+
+            setList(pokemons)
+        }
+        showPokemons()
+    }, [])
+
+    return (
+        <>
+            <h1>Pokedex</h1>
+
+            <div className="poke-list">
+                {list.map((pokemon, index) => (
+                    <div key={index}>
+                        <img src={pokemon.sprites.other.dream_world.front_default} alt={pokemon.name} />
+                      
+                    </div>
+                ))}
+
+            </div>
+
+        </>
+    )
 }
